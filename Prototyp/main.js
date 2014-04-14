1ec684a882a9be979f4507b04999d4767e9342a4
Andreas Buck <ab-132097@hs-weingarten.de>
2014-04-14 17:05:54 +0200

    Funktionen:    Ebene + Textur    Tastatureingabe

--- Prototyp/main.js
+++ Prototyp/main.js
@@ -31,34 +31,113 @@ function main ()
     scene.add(lightSource);
    
     camera = new THREE.PerspectiveCamera(90, window.innerWidth/window.innerHeight, 1, 9000);
-    //camera.position.x = 1000;
-    //camera.position.y = 1000;
-    camera.position.z = 1000;
-    //camera.rotation.y = 0.3;
-    //camera.lookAt(new THREE.Vector3(0.0 ,0.0, 0.0));
-    
-    var playingFieldMaterial = new THREE.MeshBasicMaterial({
-        color: 0xFF0000,
-        ambient: 0xFFFFFF,
-        side: THREE.DoubleSide,
-        wireframe: true
-    });  
+    camera.position.z = 300;
+    camera.position.y = 0;
+    camera.position.x = 420;
+    camera.rotation.x = 0.5;
 
-    playingFieldMesh = new THREE.Mesh(new THREE.PlaneGeometry(1000, 1000, 10, 10), playingFieldMaterial);
-    playingFieldMesh.rotation.x -= 0.7;
-//    playingFieldMesh.rotation.z -= 0.3;
+    playingFieldTexture = new THREE.ImageUtils.loadTexture('Grid.png');
+    playingFieldMaterial = new THREE.MeshBasicMaterial({
+        color: 0x009900,
+        side: THREE.DoubleSide,
+        map: playingFieldTexture
+    });  
+    
+    playingFieldHighlightMaterial = new THREE.MeshBasicMaterial({
+        color: 0xcccccc,
+        side: THREE.DoubleSide,
+        map: playingFieldTexture
+    }); 
+
+    
+    playingFieldMesh = new THREE.Mesh(new THREE.PlaneGeometry(100, 100, 1, 1), playingFieldMaterial);
+    playingFieldArray = new Array();
+    for (var i=0.0; i<1000.0; i+=100.0)
+    {
+        playingFieldArray[i/100] = new Array();
+        for (var j=0.0; j<1000.0; j+=100.0)
+        {
+            var playingFieldCell = playingFieldMesh.clone();
+            playingFieldCell.position = new THREE.Vector3(i, j, 0.0);
+            playingFieldArray[i/100][j/100] = playingFieldCell;
+            scene.add(playingFieldCell);
+        }
+    }
     scene.add(playingFieldMesh);
+    document.addEventListener('keydown', onKeyDown, false);
+    mouseClicked = false;
+    mouseX = 0.0;
+    mouseY = 0.0;
+    selectedField = new THREE.Vector2(5,0);
+    hightlightCell(selectedField.x,selectedField.y);
 }
 
-//function animate() {
-//    requestAnimationFrame( animate );           
-//    render();
-//}
+function hightlightCell()
+{
+    playingFieldArray[selectedField.x][selectedField.y].material = playingFieldHighlightMaterial;
+}
+
+function resetCellMaterial()
+{
+    playingFieldArray[selectedField.x][selectedField.y].material = playingFieldMaterial;
+}
+
+function onKeyDown(event) 
+{
+    var e = event.keyCode;
+
+    switch(e) {
+        // arrow left
+        case 39:
+            resetCellMaterial();
+            selectedField.x += 1;
+            selectedField.x %= 10;
+            hightlightCell();
+            break;
+
+        // arrow right
+        case 37:
+            resetCellMaterial();
+            selectedField.x -= 1;
+            selectedField.x %= 10;
+            if (selectedField.x < 0)
+            {
+                selectedField.x = 9;
+            }
+            hightlightCell();
+            break;
+
+        // arrow up
+        case 38:
+            resetCellMaterial();
+            selectedField.y += 1;
+            selectedField.y %= 10;
+            hightlightCell();
+            break;
+
+        // arrow down
+        case 40:
+            resetCellMaterial();
+            selectedField.y -= 1;
+            selectedField.y %= 10;
+            if (selectedField.y < 0)
+            {
+                selectedField.y = 9;
+            }
+            hightlightCell();
+            break; 
+    }
+}
+
+function onMouseClick(event)
+{
+   mouseClicked = true;
+   mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+   mouseY = 1 - (event.clientY / window.innerHeight) * 2;
+}
 
 function render ()
 {
     requestAnimationFrame( render );
     renderer.render(scene, camera);
 }
-
-
