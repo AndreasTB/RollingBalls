6b24438eee4cb0bd738024ed912395c82d4c1d54
Andreas Buck <ab-132097@hs-weingarten.de>
2014-05-19 00:51:16 +0200

    Fixed: The path wasn't followed properly (ball wouldn't pass left of tower but through tower) Fixed: Balls wouldn't disappear at the end of the playing field Fixed: Fish eye distortion of the playing field Fixed: There was no check for whether there was a path for the current ball Added:    - Towers are firing at the nearest ball    - Towers have a firing rate and cooldown time    - Impact of bullets is checked against floor and balls
    Todo: more intelligent way to determine ball position relative to playing field grid
    Todo: towers need to fire at expected position rather than current
    Todo: hit points for balls missing (hit condition present)

--- Prototyp/main.js
+++ Prototyp/main.js
@@ -24,8 +24,12 @@ function main ()
     cellsize = 100.0;
     gridSizeX = 10.0;
     gridSizeY = 10.0;
-    speed = 2.0;
+    ballSpeed = 2.0;
     ballRadius = 30.0;
+    bulletSpeed = 7.0;
+    bulletRadius = 5.0;
+    towerHight = 100.0;
+    towerFireRate = 0.4;
     scene = new THREE.Scene();
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -36,11 +40,11 @@ function main ()
     lightSource.position.set(30, 20, 20);
     scene.add(lightSource);
 
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 1, 9000);
-    camera.position.z = 300;
-    camera.position.y = 0;
+    camera = new THREE.PerspectiveCamera(30, window.innerWidth/window.innerHeight, 1, 9000);
+    camera.position.z = 800;
+    camera.position.y = -700;
     camera.position.x = 420;
-    camera.rotation.x = 0.5;
+    camera.rotation.x = 0.9;
 
     playingFieldTexture = new THREE.ImageUtils.loadTexture('Grid.png');
     playingFieldMaterial = new THREE.MeshBasicMaterial({
@@ -62,6 +66,7 @@ function main ()
     playingFieldMesh = new THREE.Mesh(new THREE.PlaneGeometry(100, 100, 1, 1), playingFieldMaterial);
     playingFieldArray = new Array();
     pathFindingArray = new Array();
+    bulletArray = new Array();
     for (var i = 0.0; i<gridSizeX; i++)
     {
         towerArray[i] = new Array();
@@ -73,7 +78,7 @@ function main ()
             playingFieldArray[i][j] = playingFieldCell;
             scene.add(playingFieldCell);
 
-            // Initialisiere TowerArray mit Bullshit
+            // Initialisiere TowerArray (0.0 = leeres Feld)
             towerArray[i][j] = 0.0;
         }
     }
@@ -111,7 +116,7 @@ function onKeyDown (event)
             hightlightCell();
             break;
 
-            // arrow right
+        // arrow right
         case 37:
             resetCellMaterial();
             selectedField.x -= 1;
@@ -123,7 +128,7 @@ function onKeyDown (event)
             hightlightCell();
             break;
 
-            // arrow up
+        // arrow up
         case 38:
             resetCellMaterial();
             selectedField.y += 1;
@@ -154,22 +159,6 @@ function onKeyDown (event)
     }
 }
 
-function onMouseClick (event)
-{
-    mouseClicked = true;
-    mouseX = (event.clientX/window.innerWidth)*2-1;
-    mouseY = 1-(event.clientY/window.innerHeight)*2;
-}
-
-function isDirectionFree(currentFieldX, currentFieldY)
-{
-    if (towerArray[currentFieldX][currentFieldY] !== 0.0)
-    {
-        return false;
-    }
-    return true;
-}
-
 function determineBallPosition(ball)
 {
     return [Math.floor(ball.position.x/cellsize),
@@ -178,111 +167,28 @@ function determineBallPosition(ball)
 
 function moveBall(ballId)
 {
+    // checks whether there is a path for the current ball
+    if (pathFindingArray[ballId].length === 0)
+    {
+        return;
+    } 
+    
     var ball = ballArray[ballId];
-    currentFieldX = Math.floor(ball.position.x/cellsize);
-    currentFieldY = Math.floor(ball.position.y/cellsize);
-
-//    Versuch 1
-//    var ballposition = new THREE.Vector3();
-//    ballposition.getPositionFromMatrix( ball.matrixWorld );
-//    ball.geometry.computeBoundingBox();
-//    var boundingBox = ball.geometry.boundingBox;
-//    
-//    Versuch 2
-//    var ballPosition = new THREE.Vector3();
-//    ballPosition.subVectors( boundingBox.max, boundingBox.min );
-//    ballPosition.multiplyScalar( 0.5 );
-//    //position.add( boundingBox.min );
-//
-//
-//
-//    
-//    ballPosition.applyMatrix4( ball.matrixWorld );
-//    
-//    for (var currentFieldX=0; currentFieldX<gridSizeX; currentFieldX++)
-//    {
-//        if (ball.position.x < playingFieldArray[currentFieldX][0].position.x)
-//        {
-////            currentFieldX--;
-//            break;
-//        }
-//    }
-//    
-//    for (var currentFieldY=0; currentFieldY<gridSizeY; currentFieldY++)
-//    {
-////        if (ball.position.y < playingFieldArray[0][currentFieldY].position.y)
-////        {
-////            currentFieldY--;
-//            //break;
-//            var test= playingFieldArray[0][currentFieldY].position.y;
-////        }
-//    }   
-    
-    deltaX = pathFindingArray[ballId][0].x - currentFieldX;
-    deltaY = pathFindingArray[ballId][0].y - currentFieldY;
-    
-    if (deltaX === 0 && deltaY === 0)
+    var fieldX = pathFindingArray[ballId][0].x;
+    var fieldY = pathFindingArray[ballId][0].y;
+    var path = playingFieldArray[fieldX][fieldY].position.clone();
+    path.z = ballRadius;
+    var distance = computeDistance(ball.position, path);
+    if (distance < 1.1)
     {
         pathFindingArray[ballId].splice(0,1);
-        deltaX = pathFindingArray[ballId][0].x - currentFieldX;
-        deltaY = pathFindingArray[ballId][0].y - currentFieldY;       
-    }
+        return;
+    }   
     
-    if (deltaX < 0)
-    {
-        ball.position.x -= speed;
-        return;
-    }
-    
-    if (deltaX > 0)
-    {
-        ball.position.x += speed;
-        return;
-    }
-   
-    if (deltaY < 0)
-    {
-        ball.position.y -= speed;
-        return;
-    }
-   
-    if (deltaY > 0)
-    {
-        ball.position.y += speed;
-        return;
-    }    
+    ball.lookAt(path);
+
+    ball.translateZ( ballSpeed );
 }   
-
-function render ()
-{
-    requestAnimationFrame(render);
-    renderer.render(scene, camera);
-
-    if (ballArray.length>0)
-    {
-        for (var ballId = 0; ballId<ballArray.length; ballId++)
-        {
-            if (ballArray[ballId].position.y>0.3*gridSizeY*cellsize)
-            {
-                scene.remove(ballArray[ballId]);
-                ballArray.splice(ballId, 1);
-                pathFindingArray.splice(ballId, 1);
-            }
-            else
-            {
-                if (playingFieldChanged)
-                {
-                    for (var i = 0; i < pathFindingArray.length; i++)
-                    {
-                        findBestPath(i);
-                    }
-                }
-                moveBall(ballId);
-            }
-             playingFieldChanged = false;
-        }
-    }
-}
 
 function createTower ()
 {
@@ -297,17 +203,17 @@ function createTower ()
 
     if (towerArray[x][y] === 0.0)
     {
-        var cube = new THREE.Mesh(new THREE.BoxGeometry(40, 40, 100, 1, 1, 1), cubeMaterial);
+        var cube = new THREE.Mesh(new THREE.BoxGeometry(40, 40, towerHight, 1, 1, 1), cubeMaterial);
         cube.position = playingFieldArray[x][y].position.clone();
-        cube.position.z += 20;
+        cube.position.z += towerHight/2;
 
-        towerArray[x][y] = cube;
+        towerArray[x][y] = {cube:cube, coolDown:0.0};
         towerCount++;
         scene.add(cube);
     }
     else
     {
-        scene.remove(towerArray[x][y]);
+        scene.remove(towerArray[x][y].cube);
         towerArray[x][y] = 0.0;
     }
     playingFieldChanged = true;
@@ -315,6 +221,7 @@ function createTower ()
 
 function findBestPath(ballId) {
     var currentLength = 100;
+    pathFindingArray[ballId] = new Array();
     for (var i = 0; i < 10; i++)
     {
         var ballPosition = determineBallPosition(ballArray[ballId]);
@@ -337,9 +244,9 @@ function createBall ()
     var randX = Math.floor((Math.random()*10));
 
     var ball = new THREE.Mesh(new THREE.SphereGeometry(ballRadius, 16, 16), ballMaterial);
-//    ball.position = playingFieldArray[randX][0].position.clone();
-    ball.position = playingFieldArray[3][0].position.clone();
-    ball.position.z += 30.0;
+    ball.position = playingFieldArray[randX][0].position.clone();
+//    ball.position = playingFieldArray[3][0].position.clone();
+    ball.position.z += ballRadius;
     ball.position.y = 1.0;
     
     pathFindingArray.push(new Array());
@@ -347,4 +254,156 @@ function createBall ()
     findBestPath(ballArray.length - 1);
 
     scene.add(ball);
+}
+
+function controlBalls()
+{
+    if (ballArray.length>0)
+    {
+        for (var ballId = 0; ballId<ballArray.length; ballId++)
+        {
+            // if the current ball has no more path to follow, it reached its destination
+            if (pathFindingArray[ballId].length === 0)
+            {
+                scene.remove(ballArray[ballId]);
+                ballArray.splice(ballId, 1);
+                pathFindingArray.splice(ballId, 1);
+            }
+            else
+            {
+                if (playingFieldChanged)
+                {
+                    for (var i = 0; i < pathFindingArray.length; i++)
+                    {
+                        findBestPath(i);
+                    }
+                }
+                moveBall(ballId);
+            }
+             playingFieldChanged = false;
+        }        
+    }
+}
+
+function fireTowers()
+{
+    // no balls no targets
+    if (ballArray.length === 0)
+    {
+        return;
+    }
+    for (var x=0.0; x<gridSizeX; x++)
+    {
+        for (var y=0.0; y<gridSizeY; y++)
+        {
+            if (towerArray[x][y] !== 0.0 && towerArray[x][y].coolDown <= 0.0)
+            {
+                // fire - TODO: add range
+                var closestBallId = findClosestBall(towerArray[x][y].cube.position);
+                // -1 means there was no ball found - holding fire
+                if (closestBallId !== -1)
+                {
+                    createBullet(towerArray[x][y].cube.position, ballArray[closestBallId].position);
+                    towerArray[x][y].coolDown = 10.0;   
+                }
+            }
+            else
+            {
+                towerArray[x][y].coolDown -= towerFireRate;
+            }
+        }
+    }
+}
+
+function createBullet (towerPosition, closestBallPosition)
+{
+    var bulletMaterial = new THREE.MeshLambertMaterial(
+        {
+            color: 0xFF0000
+        }
+    );
+
+    var bullet = new THREE.Mesh(new THREE.SphereGeometry(bulletRadius, 8, 8), bulletMaterial);
+    bullet.position = towerPosition.clone();
+    bullet.position.z = towerHight;
+
+    //bullet.up = closestBallPosition;
+    bullet.lookAt(closestBallPosition);
+    bulletArray.push(bullet);
+    scene.add(bullet);
+}
+
+function controlBullets()
+{
+    // loop through all existing bullets
+    if (bulletArray.length > 0)
+    {
+        for (var bulletId = 0; bulletId < bulletArray.length; bulletId++)
+        {
+            bulletArray[bulletId].translateZ( bulletSpeed );
+            testBulletImpact(bulletArray[bulletId]);
+        }
+    }
+}
+
+function testBulletImpact(bullet)
+{
+    // check each existing ball (shot can aim at one ball and hit another or none)
+    if (ballArray.length > 0)
+    {
+        for (var ballId = 0; ballId<ballArray.length; ballId++)
+        {
+            // if the centre of the bullet is within ballRadius - bulletRadius of the centre of the ball: ball got hit
+            if ((ballRadius - bulletRadius) > computeDistance(bullet.position, ballArray[ballId].position))
+            {
+                // ball hit
+                scene.remove(bullet);
+                return;
+            }
+        }
+    }
+    
+    // if no ball was hit: test whether z is still positiv
+    if (bullet.position.z <= 0.0)
+    {
+        // ground was hit - bullet destroyed
+        scene.remove(bullet);
+        return;
+    }
+}
+
+function computeDistance(positionFrom, positionTo)
+{
+    return Math.sqrt(Math.pow(positionFrom.x - positionTo.x, 2)
+                              + Math.pow(positionFrom.y - positionTo.y, 2)
+                              + Math.pow(positionFrom.z - positionTo.z, 2));
+}
+
+// searches the closest ball from the current tower
+function findClosestBall(towerPosition)
+{
+    var distance = 1000.0;
+    var targetBallId = -1;
+    if (ballArray.length>0)
+    {
+        for (var ballId = 0; ballId<ballArray.length; ballId++)
+        {
+            var newDistance = computeDistance(towerPosition, ballArray[ballId].position);
+            if (newDistance < distance)
+            {
+                distance = newDistance;
+                targetBallId = ballId;
+            }
+        }
+    }
+    return targetBallId;
+}
+
+function render ()
+{
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+    controlBalls();
+    fireTowers();
+    controlBullets();
 }
